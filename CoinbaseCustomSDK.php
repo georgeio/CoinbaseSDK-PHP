a68fdc360f8c9dbef206c55eaf66ad31b81bbac3
George Imoedemhe <george.imoedemhe@gmail.com>
2017-04-18 08:11:50 +0100

    Fixed ReadMe

--- CoinbaseCustomSDK.php
+++ CoinbaseCustomSDK.php
@@ -29,8 +29,8 @@ class CoinbaseCustomSDK
 
 	public function __construct($api_key = '', $api_secret	= '')
 	{
-		$this->api_key	=	$api_key;
-		$this->api_secret	=	$api_secret;
+		$this->api_key = $api_key;
+		$this->api_secret = $api_secret;
 		$this->setAccountId();
 	}
 
